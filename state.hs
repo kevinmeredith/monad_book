eafe3bedd87bf00d8bdb257c6fb9ba1c5a247d5e
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-04-24 09:00:15 -0400

    walk through and complete (w/ help) state monad.

--- state.hs
+++ state.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import Control.Monad.State
 
 -- credit: book's solution
@@ -6,7 +8,25 @@ state_obj = state $ \s -> ("a", "b")
 
 show_result :: Show a => State String a -> IO ()
 show_result st = do
-	(one, two) <- return $ runState st
-	putStrLn "First: one"
-	putStrLn "Second: two"
+	(one, two) <- return $ runState st "anything"
+	putStrLn $ "First: "  ++ (show one)
+	putStrLn $ "Second: " ++ two
 
+
+type Stack a = State [a]
+
+processAsStack :: [a] -> Stack a () -> [a]
+processAsStack xs st = snd $ runState st xs -- snd $ runState st []
+
+printList lst = putStrLn (show lst)
+
+push :: a -> Stack a ()
+push a = state $ \(xs) -> ((), a:xs)
+
+pop :: Stack a a 
+pop = state $ \(x:xs) -> (x, xs)
+
+doIt = printList $ processAsStack "ct" $ do
+	x <- pop	
+	push 'a'
+	push x
